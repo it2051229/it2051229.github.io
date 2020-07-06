71f933aff521efc6d1617fd84810e104da40176b
it2051229 <contact@it2051229.com>
2020-07-07 00:33:18 +0800

    July 7, 2020
    Added distributed folder for stable release.

---
+++ seedin/distributed/precache-manifest.f6be069ed1a2a853be55256801a0290f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c6cfaeaf9b368d0387e3e56c88408c8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c8d46852aa9f44f26cd7",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b7f6c959371c8ffcb02d",
+    "url": "./static/css/main.9e124ad1.chunk.css"
+  },
+  {
+    "revision": "c8d46852aa9f44f26cd7",
+    "url": "./static/js/2.5e3cf4f4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.5e3cf4f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f6c959371c8ffcb02d",
+    "url": "./static/js/main.0682e739.chunk.js"
+  },
+  {
+    "revision": "e8d54aa1b01165c69970",
+    "url": "./static/js/runtime-main.f3bd3402.js"
+  }
+]);
